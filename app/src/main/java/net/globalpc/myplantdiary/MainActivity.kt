759a6527a44310e445a77e304b35e030b646ce0b
Raymundo Valdez <ing.ray.valdez@gmail.com>
2022-10-31 15:41:28 -0500

    Commit inicial.

---
+++ app/src/main/java/net/globalpc/myplantdiary/MainActivity.kt
@@ -0,0 +1,18 @@
+package net.globalpc.myplantdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.globalpc.myplantdiary.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/net/globalpc/myplantdiary/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package net.globalpc.myplantdiary.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
